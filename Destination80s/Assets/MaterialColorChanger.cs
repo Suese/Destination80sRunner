9739f5b7f27f6afa3eefa6a231b8b9513d30dcc6
TheSuese <dan@danmckinnon.net>
2017-10-14 17:36:04 -0700

    Added a color changing material

---
+++ Destination80s/Assets/MaterialColorChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialColorChanger : MonoBehaviour {
+
+	public Material material;
+	public Gradient gradient;
+	public float speed;
+	public string colorUniform;
+
+	void Update () {
+		material.SetColor( colorUniform, gradient.Evaluate( Mathf.Repeat(Time.time * speed,1) ) );
+	}
+}
