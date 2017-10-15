bfe90ed030a178f6bea5b3c5608f9231b70cddaf
TheSuese <dan@danmckinnon.net>
2017-10-15 12:38:21 -0700

    Added a ship sprite

--- Destination80s/Assets/RotateConstantly.cs
+++ Destination80s/Assets/RotateConstantly.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class RotateConstantly : MonoBehaviour {
 
+	public enum Axis{
+		x,y,z
+	}
 	public float speed = 1.0f;
+	public Axis axis = Axis.y;
 	float r = 0;
 	void Update () {
 		r += Time.deltaTime * speed;
 		if( r >= 360 ){
 			r -= 360;
 		}
-		transform.localRotation = Quaternion.Euler( 0, r, 0 );
+		switch( axis ){
+			case Axis.x:
+				transform.localRotation = Quaternion.Euler( r, 0, 0 );
+			break;
+			case Axis.y:
+				transform.localRotation = Quaternion.Euler( 0, r, 0 );
+			break;
+			case Axis.z:
+				transform.localRotation = Quaternion.Euler( 0, 0, r );
+			break;
+		}
 	}
 }
